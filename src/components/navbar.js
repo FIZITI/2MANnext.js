e17c58cabf8ace74908c3560b03bd2e4b9f86d67
FIZITI <bormandeny3@gmail.com>
2023-08-21 21:22:48 +0300

    /# link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -20,14 +20,14 @@ export default function Navbar() {
           </Link>
           <nav className="lmd:hidden">
             <ul className="flex space-x-[38px] ul-menu mr-[45px]">
-              <li className="active-link"><Link href="#HomeBlock">Главная</Link></li>
-              <li className="hover-menu-link"><Link href="#Service">Услуги</Link></li>
+              <li className="active-link"><Link href="/#HomeBlock">Главная</Link></li>
+              <li className="hover-menu-link"><Link href="/#Service">Услуги</Link></li>
               <li className="hover-menu-link"><Link href="/portfolio">Портфолио</Link></li>
-              <li className="hover-menu-link"><Link href="#AboutUs">О&nbsp;нас</Link></li>
-              <li className="hover-menu-link"><Link href="#Team">Команда</Link></li>
-              <li className="hover-menu-link"><Link href="#Jobs">Цены</Link></li>
+              <li className="hover-menu-link"><Link href="/#AboutUs">О&nbsp;нас</Link></li>
+              <li className="hover-menu-link"><Link href="/#Team">Команда</Link></li>
+              <li className="hover-menu-link"><Link href="/#Jobs">Цены</Link></li>
               <li className="hover-menu-link"><Link href="#">Блог</Link></li>
-              <li className="hover-menu-link"><Link href="#Footer">Контакты</Link></li>
+              <li className="hover-menu-link"><Link href="/#Footer">Контакты</Link></li>
             </ul>
           </nav>
           <button className="hidden lmd:flex justify-center items-center w-[50px] h-[50px]">
